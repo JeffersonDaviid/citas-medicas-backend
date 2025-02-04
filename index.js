ed3acd237f1220286e6ed9532a65e6b9a30d5aae
Jefferson <jefferson.chileno@epn.edu.ec>
2025-02-03 19:49:50 -0500

    link de produccion

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var mongoose = require('mongoose');
-var port = '3600';
-mongoose.Promise = global.Promise;
-var app = require('./app');
+var mongoose = require('mongoose')
+var port = '3600'
+mongoose.Promise = global.Promise
+var app = require('./app')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 mongoose
-  .connect('mongodb://localhost:27017/citas-medicas', {})
-  .then(() => {
-    console.log('Conectado a la base de datos');
-    app.listen(port, () => {
-      console.log('Servidor corriendo en http://localhost:' + port);
-    });
-  })
-  .catch((err) => console.log(err));
+	.connect(
+		'mongodb://mongo:xFfDFFUxCLxClEOJqZfulXEzggfquzrl@monorail.proxy.rlwy.net:19983',
+		{}
+	)
+	.then(() => {
+		console.log('Conectado a la base de datos')
+		app.listen(port, () => {
+			console.log('Servidor corriendo en http://localhost:' + port)
+		})
+	})
+	.catch((err) => console.log(err))
